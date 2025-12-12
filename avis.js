b6b76f69877c79226ca658d84b8a47d9a67ad19c
Mahmoud-Diop <mouhamedmahmoud2001@gmail.com>
2025-12-12 03:45:39 +0000

    ajout du champs note(nombre d'etoile) d'un piece

--- avis.js
+++ avis.js
@@ -7,7 +7,7 @@ export function ajoutListenersAvis() {
      piecesElements[i].addEventListener("click", async function (event) {
  
         const id = event.target.dataset.id;
-        const reponse = await fetch(`http://localhost:8081/pieces/${id}/avis`);
+        const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
         const avis = await reponse.json();
         const pieceElement = event.target.parentElement;
 
@@ -27,19 +27,14 @@ export function ajoutListenersAvis() {
     formulaireAvis.addEventListener("submit", function (event) {
     event.preventDefault();
     // Création de l’objet du nouvel avis.
-
-
     const avis = {
         pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-        utilisateur: event.target.querySelector("[name=utilisateur").value,
+        utilisateur: event.target.querySelector("[name=utilisateur]").value,
         commentaire: event.target.querySelector("[name=commentaire]").value,
+        nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
     };
     // Création de la charge utile au format JSON
-
-
     const chargeUtile = JSON.stringify(avis);
-
-    
     // Appel de la fonction fetch avec toutes les informations nécessaires
     fetch("http://localhost:8081/avis", {
         method: "POST",
